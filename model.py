f86f786943bcd923fe13b1a3507be934a7911d6b
Hackbright Student <info@hackbrightacademy.com>
2014-02-26 12:59:27 -0800

    Connecting to database. Bugs

--- model.py
+++ model.py
@@ -1,15 +1,19 @@
-#import sqlite3
+import sqlite3
 
-ADMIN_USER="hackbright"
-ADMIN_PASSWORD=5980025637247534551
+# ADMIN_USER="hackbright"
+# ADMIN_PASSWORD=5980025637247534551
+#TODO - right now CONN and DB are global variables. 
+# It is better practice to have these function calls inside a function that is
+# called from authenticate.
+CONN = sqlite3.connect("thewall.db")
+DB = CONN.cursor()
 
 def authenticate(username, password):
-    # CONN = sqlite3.connect("thewall.db")
-    # DB = CONN.cursor()
-    # query = """SELECT username, password FROM users WHERE username = ? 
-    #             and password = ?"""
-    # row = DB.execute(query, (username, hash(password)))            
-    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-        return ADMIN_USER
+    query = """SELECT username, password FROM users WHERE username = ? 
+                and password = ?"""
+    DB.execute(query, (username, hash(password)))            
+    row = DB.fetchone()
+    if username == row[0] and hash(password) == row[1]:
+        return username
     else:
         return None
