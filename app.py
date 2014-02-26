8a1040e6dd7fa5418035b7175b69291b8865c0b7
Hackbright Student <info@hackbrightacademy.com>
2014-02-26 12:43:18 -0800

    Logs in and clears session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import model
 
 app = Flask(__name__)
@@ -6,13 +6,37 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return render_template("loggedin.html", username=session['username'])
+        #"User %s is logged in!" % session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    username = model.authenticate(username, password)
+
+    if username != None:
+        flash("User authenticated")
+        session['username'] = username
+        print session['username']
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+
+    return redirect(url_for("index"))    
+
+    #return render_template("login.html")
+
+@app.route("/close")
+def end_session():
+    session.clear()
+
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
 
--- model.py
+++ model.py
@@ -1,5 +1,15 @@
+#import sqlite3
+
 ADMIN_USER="hackbright"
 ADMIN_PASSWORD=5980025637247534551
 
 def authenticate(username, password):
-    return False
+    # CONN = sqlite3.connect("thewall.db")
+    # DB = CONN.cursor()
+    # query = """SELECT username, password FROM users WHERE username = ? 
+    #             and password = ?"""
+    # row = DB.execute(query, (username, hash(password)))            
+    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
+        return ADMIN_USER
+    else:
+        return None
